2fe363fa08c17337bd23d412f831438d6ab4a137
robbym7 <robbymarshall77@gmail.com>
2025-05-25 14:28:41 -0700

    update

--- IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/Actor.h
+++ IceMan-skeleton-windows/IceMan-skeleton-windows/IceMan/Actor.h
@@ -14,14 +14,28 @@ public:
 
 private:
     bool alive;
-
-
-
 };
+
+class IceMan : public Actor {};
+
+class Ice : public Actor{};
+
+class Boulders : public Actor{};
+
+class Squirt : public Actor{};
+
+class OilBarrel : public Actor{};
+
+class GoldNugget : public Actor{};
+
+class SonarKit : public Actor{};
+
+class WaterPool : public Actor{};
 
 class Protester: public Actor {
 public:
 };
+
 class HardcoreProtester: public Protester {
 public:
 };
